cfd606c3e4dbd8481a862e86859f4c5ff1f07671
Anuja Kumari <76772223+Anujakumari@users.noreply.github.com>
2021-08-12 21:16:40 +0530

    second assignment added

---
+++ Assignment/EvenOddCheck.java
@@ -0,0 +1,18 @@
+
+package com.company;
+
+import java.util.Scanner;
+
+public class EvenOddCheck {
+
+    public static void main(String[] args) {
+        Scanner input  =  new Scanner(System.in);
+        int num = input.nextInt();
+        if (num%2==0){
+            System.out.println(num + " is even" );
+        }
+        else {
+            System.out.println(num + " is odd");
+        }
+    }
+}
