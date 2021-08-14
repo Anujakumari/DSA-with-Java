8f87074752d13df365742108d5898d096e0c0242
Anuja Kumari <76772223+Anujakumari@users.noreply.github.com>
2021-08-14 21:36:59 +0530

    Example of Conditionals added

---
+++ Java_code/Conditionals.java
@@ -0,0 +1,29 @@
+package com.company;
+public class Conditionals {
+
+    public static void main(String[] args) {
+        
+        // if-else example
+
+        int salary = 22500;
+        if (salary > 10000) {
+            salary = salary + 2000;
+        } else {
+           salary = salary + 1000;
+        }
+      
+
+        // multiple if-else example
+
+//        if (salary > 10000) {
+//            salary += 2000;
+//        } else if (salary > 20000) {
+//            salary += 3000;
+//        } else {
+//            salary += 1000;
+//        }
+//
+//        System.out.println(salary);
+
+    }
+}
